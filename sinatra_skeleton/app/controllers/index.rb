f408b230a4ed3e1bc3c2c17d8f6dcddd891f9dba
thejessleigh <j.l.unrein@gmail.com>
2014-02-19 15:42:34 -0600

    Created basic controller and views.
    Everything clicks through. Need to have sessions functionality and databases
    next.

--- sinatra_skeleton/app/controllers/index.rb
+++ sinatra_skeleton/app/controllers/index.rb
@@ -1,4 +1,51 @@
 get '/' do
+ redirect '/login'
+end
+
+get '/login' do
+  #login page
+  #view will have form with user and pass fields
+
+
+  #check to see if user and pass match an existing database record
+  #if user and pass exist - add user_id to the session
+  #needs to have a link to create an account page
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/login' do
+  redirect '/secret'
+end
+
+get '/logout' do
+  #remove user_id from the session
+  redirect '/login'
+end
+
+get '/create' do
+  erb :create_user
+end
+
+post '/create' do
+  redirect '/secret'
+end
+
+get '/secret' do
+  # if user_id in session
+  # one view
+  # else
+  # redirect '/login'
+  #end
+  erb :secret
+end
+
+
+#need to support the following behaviors
+
+#login
+#logout
+#create account
+#view secret page
+#redirect user back to login if unauthenticated user
+  #attempts to access secret page
