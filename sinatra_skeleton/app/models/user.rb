72a86a16ed7f1cc9b696f096e2cd50c02ad0813c
thejessleigh <j.l.unrein@gmail.com>
2014-02-19 16:00:10 -0600

    Have a working but totally unsecure skeleton
    Does not check for sessions yet.

--- sinatra_skeleton/app/models/user.rb
+++ sinatra_skeleton/app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
 
   def self.authenticate(email, password)
-
+    if user = User.where(email: email).first
+        user.password == password
+      "You typed in the right password"
+    else
+      "you did not type in the right password"
+    end
   end
 
 end
